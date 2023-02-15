b075c3569faa96ce046ca906ef9c7efc982cf0bf
ftrapero <ftrapero@alfredsmart.com>
2023-02-15 19:20:27 +0100

    Refactor to modbus light device

--- alfred_device_manager/infrastructure/devices_integrations/modbus_light.py
+++ alfred_device_manager/infrastructure/devices_integrations/modbus_light.py
@@ -1,6 +1,6 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
-from alfred_device_manager.domain.device import Device, DeviceCommand
+from alfred_device_manager.domain.device import Device, DeviceCommand, DeviceType
 
 
 @dataclass
@@ -8,18 +8,21 @@ class ModbusLight(Device):
     """
     Modbus light device integration
     """
-    _address: int
+    address: int = field(init=False)
 
     def __post_init__(self):
         try:
-            self._address = int(self.additional_info["address"])
+            self.address = int(self.additional_info["address"])
         except KeyError as exception:
             raise ValueError("Missing address in additional_info") from exception
         except ValueError as exception:
             raise ValueError("Invalid address in additional_info") from exception
 
+        if self.device_type != DeviceType.LIGHT_MODBUS:
+            raise ValueError("Invalid device type")
+
     def execute_command(self, command: DeviceCommand):
         if command == DeviceCommand.TURN_ON:
-            print(f"Sending 1 to modbus address {self._address}")
+            print(f"Sending 1 to modbus address {self.address}")
         elif command == DeviceCommand.TURN_OFF:
-            print(f"Sending 0 to modbus address {self._address}")
+            print(f"Sending 0 to modbus address {self.address}")
